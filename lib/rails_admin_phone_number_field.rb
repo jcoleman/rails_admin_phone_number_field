2fcab28e3c7650a85984742707d63e155597f7ef
James Coleman <jtc331@gmail.com>
2013-05-16 16:14:25 -0400

    Remove dead code/extra logging.

--- lib/rails_admin_phone_number_field.rb
+++ lib/rails_admin_phone_number_field.rb
@@ -14,7 +14,6 @@ module RailsAdmin
           RailsAdmin::Config::Fields::Types::register(self)
 
           register_instance_option :partial do
-            Rails.logger.debug "partial phone value"
             :form_phone_number_field
           end
 
@@ -23,13 +22,11 @@ module RailsAdmin
           end
 
           register_instance_option :default_value do
-            Rails.logger.debug "default phone value"
             nil
           end
 
           register_instance_option :formatted_value do
             formattable = value
-            Rails.logger.debug "formatting phone value: #{formattable}"
             if formattable
               formattable.phony_formatted(:format => number_format, :spaces => '.')
             else
@@ -38,13 +35,11 @@ module RailsAdmin
           end
 
           register_instance_option :pretty_value do
-            Rails.logger.debug "pretty phone value"
             formatted_value.presence || ' - '
           end
 
           # output for printing in export view (developers beware: no bindings[:view] and no data!)
           register_instance_option :export_value do
-            Rails.logger.debug "export phone value"
             pretty_value
           end
 
@@ -56,15 +51,3 @@ module RailsAdmin
     end
   end
 end
-
-#RailsAdmin::Config::Fields.register_factory do |parent, properties, fields|
-#  Rails.logger.debug "checking #{properties}"
-#  if properties[:type] == :phone_number_field
-#    fields << RailsAdmin::Config::Fields::Types::PhoneNumberField.new(parent, properties[:name], properties)
-#    true
-#  else
-#    false
-#  end
-#end
-
-
